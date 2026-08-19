1114268ad6476731faaf01996eef8211415e3b45
amirms5180-ui <amirm.s.5180@gmail.com>
2026-08-19 16:28:34 +0330

    change

--- src/App.js
+++ src/App.js
@@ -1,103 +1,100 @@
 import { useState } from "react";
 
-const wins = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-function winnerHandler(squares) {
-  const winningLine = wins.find((win) => {
-    return (
-      squares[win[0]] &&
-      squares[win[0]] === squares[win[1]] &&
-      squares[win[0]] === squares[win[2]]
-    );
-  });
-
-  if (winningLine) {
-    return squares[winningLine[0]];
-  }
-
-  return null;
-}
 
 export default function App() {
-  const [ss, setSs] = useState(false);
+  const [ss, setss] = useState(false);
   const [squares, setSquares] = useState(Array(9).fill(null));
-
+  const wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  
+  function winnerHandler(squares) {
+    const winningLine = wins.find((win) => {
+      return (
+        squares[win[0]] &&
+        squares[win[0]] === squares[win[1]] &&
+        squares[win[0]] === squares[win[2]]
+      );
+    });
+  
+    if (winningLine) {
+      return {
+       player: squares[winningLine[0]],
+       line:winningLine}
+    }
+    
+    return null;
+  }
+  
+  function resetHandler (){
+    setSquares(Array(9).fill(null));
+    setss(false);
+     
+  
+  }
+  
   function handleClick(index) {
     if (winnerHandler(squares)) {
       return;
     }
-
+    
     if (squares[index]) {
       return;
     }
-
+    
     const nextSquares = [...squares];
-
+    
     if (ss === false) {
       nextSquares[index] = "O";
     } else {
       nextSquares[index] = "X";
     }
-
+    
     setSquares(nextSquares);
-    setSs(!ss);
+    setss(!ss);
   }
-
+  function wichPlayer(){
+    const winner = winnerHandler(squares);
+    if(winner)
+      if( winner=== "O"){return "player O is win "}
+    else{return"player X is win"}
+    
+    
+  }
   const winner = winnerHandler(squares);
 
   return (
-      <div>
+      <div className="all">
 
     <div className="table">
-      <button className="squad" onClick={() => handleClick(0)}>
-        {squares[0]}
-      </button>
+  {squares.map((square, index) => (
+    <button
+      className="squad"
+      style={{
+        backgroundColor: winner?.line.includes(index) ? "cadetblue" : ""
+      }}
+      onClick={() => handleClick(index)}
+    >
+      {square}
+    </button>
+  ))}
+</div>
 
-      <button className="squad" onClick={() => handleClick(1)}>
-        {squares[1]}
-      </button>
-
-      <button className="squad" onClick={() => handleClick(2)}>
-        {squares[2]}
-      </button>
-
-      <button className="squad" onClick={() => handleClick(3)}>
-        {squares[3]}
-      </button>
-
-      <button className="squad" onClick={() => handleClick(4)}>
-        {squares[4]}
-      </button>
-
-      <button className="squad" onClick={() => handleClick(5)}>
-        {squares[5]}
-      </button>
-
-      <button className="squad" onClick={() => handleClick(6)}>
-        {squares[6]}
-      </button>
-
-      <button className="squad" onClick={() => handleClick(7)}>
-        {squares[7]}
-      </button>
-
-      <button className="squad" onClick={() => handleClick(8)}>
-        {squares[8]}
-      </button>
-
-    </div>
-       <div className="winner">
-      {winner && <h2>{winner} Winner!</h2>}
+    
+        <div className="winner ">
+          <button className="resetbtn"  onClick={()=>{resetHandler()}}>Reset</button>
         </div>
+       <div className="winner">
+      { wichPlayer && <h2>{ wichPlayer()}  </h2>}
+        </div>
+        
       </div>
   );
 }
